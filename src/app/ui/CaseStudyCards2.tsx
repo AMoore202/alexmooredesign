6cf2ccebf943e4fe7b319c1a9c0b6fe77a193df2
Alexander Moore <alexandermoore202@gmail.com>
2024-12-06 22:19:26 -0500

    Removed old code

--- src/app/ui/CaseStudyCards2.tsx
+++ src/app/ui/CaseStudyCards2.tsx
@@ -6,13 +6,7 @@ import Link from "next/link";
 import { useState } from "react";
 
 export default function CaseStudyCards2() {
-  // const [imagesLoaded, setImagesLoaded] = useState(false);
   const [caseStudyImage1Loaded, setCaseStudyImage1Loaded] = useState(false);
-
-  // const handleButtonClick = () => {
-  //   setImagesLoaded(!imagesLoaded);
-  //   console.log("Button Pressed");
-  // };
 
   const handleImageLoad = () => {
     setCaseStudyImage1Loaded(true);
@@ -133,97 +127,5 @@ export default function CaseStudyCards2() {
     </div>
   );
 
-  return (
-    <>
-      {/* <button onClick={handleButtonClick}>Load Images</button> */}
-      {caseStudyContent}
-      {/* {imagesLoaded && (
-        <div id="casestudies" className={styles.casestudies}>
-          <Link
-            className={`${styles.casestudy} ${styles.casestudyloadflight}`}
-            href="./loadflightcasestudy"
-          >
-            <div
-              className={`${styles.casestudystub} ${styles.casestudystubloadflight}`}
-            >
-              <p className={styles.casestudystubtext}>
-                Refreshed UI for loading luggage on a flight ✈️
-              </p>
-            </div>
-            <div
-              className={`${styles.casestudycontent} ${styles.casestudycontentloadflight}`}
-            >
-              <div
-                className={`${styles.casestudyimage} ${styles.casestudyimageloadflight}`}
-              >
-                <Image
-                  src="/images/loadflight/loadsuccessmockup.png"
-                  layout="responsive"
-                  width={570}
-                  height={840}
-                  alt="Mockup of a hand scanner displaying a load success screen"
-                  priority
-                />
-              </div>
-            </div>
-          </Link>
-          <Link
-            className={`${styles.casestudy} ${styles.casestudybagsummary}`}
-            href="./bagsummarycasestudy"
-          >
-            <div
-              className={`${styles.casestudystub} ${styles.casestudystubbagsummary}`}
-            >
-              <p className={styles.casestudystubtext}>
-                Telling the story of a lost bag 💼
-              </p>
-            </div>
-            <div
-              className={`${styles.casestudycontent} ${styles.casestudycontentbagsummary}`}
-            >
-              <div
-                className={`${styles.casestudyimage} ${styles.casestudyimagebagsummary}`}
-              >
-                <Image
-                  src="/images/bagsummary/bagsummarymobilemockup.png"
-                  layout="responsive"
-                  width={540}
-                  height={864}
-                  alt="Mockup of a phone showing a baggage tracking screen"
-                  priority
-                />
-              </div>
-            </div>
-          </Link>
-          <Link
-            className={`${styles.casestudy} ${styles.casestudybids}`}
-            href="./bidscasestudy"
-          >
-            <div
-              className={`${styles.casestudystub} ${styles.casestudystubbids}`}
-            >
-              <p className={styles.casestudystubtext}>
-                TV displays to modernize the bag room 📺
-              </p>
-            </div>
-            <div
-              className={`${styles.casestudycontent} ${styles.casestudycontentbids}`}
-            >
-              <div
-                className={`${styles.casestudyimage} ${styles.casestudyimagebids}`}
-              >
-                <Image
-                  src="/images/bids/bidsmockup.png"
-                  layout="responsive"
-                  width={728}
-                  height={512}
-                  alt="Mockup of a tv information display"
-                />
-              </div>
-            </div>
-          </Link>
-        </div>
-      )} */}
-    </>
-  );
+  return caseStudyContent;
 }
