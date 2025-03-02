5a7995052d2516bc45a4e85457fa8be35c6b4182
Alexander Moore <alexandermoore202@gmail.com>
2025-03-02 16:45:48 -0500

    Updated menu links

--- src/app/ui/MobileNavbar.tsx
+++ src/app/ui/MobileNavbar.tsx
@@ -23,11 +23,16 @@ export default function MobileNavbar() {
       initial={{ opacity: 0 }}
       animate={{ opacity: 100 }}
     >
-      <Link className={styles.navitem} href="#casestudies" onClick={closeMenu}>
-        My Work
+      <Link className={styles.navitem} href="./" onClick={closeMenu}>
+        Home
       </Link>
-      <Link className={styles.navitem} href="#aboutme" onClick={closeMenu}>
-        About Me
+      <Link
+        className={styles.navitem}
+        href="https://drive.google.com/file/d/1WeH_3IOLAv4ElxBJxCaaKpjDLp5_NLK7/view?usp=sharing"
+        target="_blank"
+        onClick={closeMenu}
+      >
+        Resume
       </Link>
       <Link className={styles.navitem} href="#footer" onClick={closeMenu}>
         Contact
