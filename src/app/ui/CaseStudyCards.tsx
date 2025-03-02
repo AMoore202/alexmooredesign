2f8894b3dce260fba190210d63a025d0a11b96e9
Alexander Moore <alexandermoore202@gmail.com>
2025-03-02 16:37:40 -0500

    Removed unneeded code

--- src/app/ui/CaseStudyCards.tsx
+++ src/app/ui/CaseStudyCards.tsx
@@ -2,7 +2,6 @@
 
 import styles from "./CaseStudyCards.module.css";
 import Image from "next/image";
-import Link from "next/link";
 import { useState } from "react";
 import PasswordModal from "./PasswordModal";
 
