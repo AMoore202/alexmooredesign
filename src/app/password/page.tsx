c446a7d02a572b3b5d6cc207229436b350c8eb30
Alexander Moore <alexandermoore202@gmail.com>
2025-03-24 22:11:21 -0400

    Added suspense to password page

--- src/app/password/page.tsx
+++ src/app/password/page.tsx
@@ -4,6 +4,7 @@ import styles from "./password.module.css";
 import Link from "next/link";
 import { useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 export default function Page() {
   const [input, setInput] = useState("");
@@ -34,36 +35,38 @@ export default function Page() {
   };
 
   return (
-    <div id="modal-overlay" className={styles.modal}>
-      <div className={styles.modalcontainer}>
-        <div className={styles.modalcopy}>
-          <h2 className={styles.title}>The good stuff awaits...</h2>
-          <p className={styles.instructiontext}>
-            <span>
-              Case studies are password-protected for confidentiality. Want to
-              take a look?
-            </span>
-            <Link href="mailto:alexandermoore202@gmail.com">
-              Shoot me an email!
-            </Link>
-          </p>
-        </div>
-        <form onSubmit={handleSubmit} className={styles.form}>
-          <div className={styles.inputwrapper}>
-            <input
-              className={`${styles.input} ${error ? styles.inputError : ""}`}
-              type="password"
-              placeholder="Password"
-              value={input}
-              onChange={(e) => setInput(e.target.value)}
-            />
-            {error && <p className={styles.error}>{error}</p>}
+    <Suspense fallback={<div>Loading...</div>}>
+      <div id="modal-overlay" className={styles.modal}>
+        <div className={styles.modalcontainer}>
+          <div className={styles.modalcopy}>
+            <h2 className={styles.title}>The good stuff awaits...</h2>
+            <p className={styles.instructiontext}>
+              <span>
+                Case studies are password-protected for confidentiality. Want to
+                take a look?
+              </span>
+              <Link href="mailto:alexandermoore202@gmail.com">
+                Shoot me an email!
+              </Link>
+            </p>
           </div>
-          <button type="submit" className={styles.button}>
-            Submit
-          </button>
-        </form>
+          <form onSubmit={handleSubmit} className={styles.form}>
+            <div className={styles.inputwrapper}>
+              <input
+                className={`${styles.input} ${error ? styles.inputError : ""}`}
+                type="password"
+                placeholder="Password"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+              />
+              {error && <p className={styles.error}>{error}</p>}
+            </div>
+            <button type="submit" className={styles.button}>
+              Submit
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
