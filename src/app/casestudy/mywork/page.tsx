65ef861f50930e252b9ab5214131a5b0e5f95ac4
Alexander Moore <alexandermoore202@gmail.com>
2025-06-01 19:04:50 -0400

    Added in second section

--- src/app/casestudy/mywork/page.tsx
+++ src/app/casestudy/mywork/page.tsx
@@ -31,13 +31,46 @@ export default function Page() {
             mockups={[
               {
                 label: "New Design",
-                image: "/images/work/smartsuiteloginnew.webp",
+                image: "/images/work/smartsuitelogin/newdesign.webp",
                 alt: "New SmartSuite Login Screen Design",
               },
               {
                 label: "Old Design",
-                image: "/images/work/smartsuiteloginold.webp",
+                image: "/images/work/smartsuitelogin/olddesign.webp",
                 alt: "Old SmartSuite Login Screen Design",
+              },
+            ]}
+          />
+        </div>
+        <div className={styles.section}>
+          <div className={styles.description}>
+            <h2 className={styles.sectiontitle}>Virtual Record Collection</h2>
+            <p className={styles.summary}>
+              My mom and I are big music fans and have both started collecting
+              vinyl. Records make the perfect gift for any occasion but it is
+              always a challenge to know which records we each already have. As
+              a fun side project, I designed an app that would serve as a
+              digital copy of our record collections so we could easily see what
+              we do and do not have.
+            </p>
+          </div>
+          <MockupSwitcher
+            mockups={[
+              {
+                label: "Collection Screen",
+                image:
+                  "/images/work/virtualrecordcollection/collectionscreen.webp",
+                alt: "Screenshot of records in a collection",
+              },
+              {
+                label: "Launch Screen",
+                image: "/images/work/virtualrecordcollection/launchscreen.webp",
+                alt: "Initial launch screen to choose a collection",
+              },
+              {
+                label: "Empty State",
+                image: "/images/work/virtualrecordcollection/emptystate.webp",
+                alt: "Screenshot of an empty collection",
               },
             ]}
           />
